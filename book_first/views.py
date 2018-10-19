ae2142bd3c346d75d7a33cb443e1ae280c16d4d6
TuringEmmy <yonglonggeng@163.com>
2018-10-19 16:32:21 +0800

    get books and add a book databases django_study is ok

--- book_first/views.py
+++ book_first/views.py
@@ -9,8 +9,8 @@ from django.views import View
 from book_first.models import BookInfo
 
 
+# /books/
 class BookListView(View):
-
     # 获取所有图书的数据
     def get(self, request):
         # 从数据库中查询所有的图数据
@@ -33,8 +33,7 @@ class BookListView(View):
         return JsonResponse(books_list, safe=False)
 
     # 新增一本图书的数据
-    def post(self,request):
-
+    def post(self, request):
         req_data = request.body
         # 注意这里接收到的数据是bytes
         json_str = req_data.decode()
@@ -45,3 +44,19 @@ class BookListView(View):
         btitle = req_dict.get('btitle')
         bpub_date = req_dict.get('bpub_date')
 
+        # 创建一本新图书并添加到数据库
+        book = BookInfo.objects.create(
+            btitle=btitle,
+            bpub_date=bpub_date
+        )
+
+        # 返回响应:201新增图书数据
+        book_dict = {
+            'id': book.id,
+            'btitle': book.btitle,
+            'bread': book.bread,
+            'bcomment': book.bcomment,
+            'image': book.image.url if book.url else ""
+        }
+
+        return JsonResponse(book_dict, status=201)
