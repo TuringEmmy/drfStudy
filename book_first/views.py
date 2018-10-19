82eeb1d4589eed1ad3fc932d5be0548f3a83957d
TuringEmmy <yonglonggeng@163.com>
2018-10-19 16:10:59 +0800

    get data is ok

--- book_first/views.py
+++ book_first/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http import JsonResponse
 from django.shortcuts import render
 
@@ -8,6 +10,8 @@ from book_first.models import BookInfo
 
 
 class BookListView(View):
+
+    # 获取所有图书的数据
     def get(self, request):
         # 从数据库中查询所有的图数据
         books = BookInfo.objects.all()
@@ -27,4 +31,17 @@ class BookListView(View):
         # 返回响应:200
 
         return JsonResponse(books_list, safe=False)
+
+    # 新增一本图书的数据
+    def post(self,request):
+
+        req_data = request.body
+        # 注意这里接收到的数据是bytes
+        json_str = req_data.decode()
+        # 将字符串转换成python字典
+        req_dict = json.loads(json_str)
+
+        # 获取数据
+        btitle = req_dict.get('btitle')
+        bpub_date = req_dict.get('bpub_date')
 
