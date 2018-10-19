f8c8f2935bf8e6906dccb87d84b05042275cf7d7
TuringEmmy <yonglonggeng@163.com>
2018-10-19 15:59:52 +0800

    edit view for BookListView

---
+++ book_first/urls.py
@@ -0,0 +1,11 @@
+# author        TuringEmmy 
+# createtime    18-10-19  下午2:56
+# coding=utf-8
+# doc           PyCharm
+from django.conf.urls import url
+
+from book_first import views
+
+urlpatterns = [
+    url(r'books/',views.BookListView.as_view())
+]
