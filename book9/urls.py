9a300b62d6486bf3764a1546cdf3ac7b1936cab0
TuringEmmy <yonglonggeng@163.com>
2018-10-23 10:02:26 +0800

    the first route is over to <BookInfoSetViewSet>

--- book9/urls.py
+++ book9/urls.py
@@ -1,3 +1,13 @@
 # author    python
 # time      18-10-23 上午9:58
 # project   drtStudy
+from django.conf.urls import url
+
+from book9 import views
+
+urlpatterns = [
+    url(r'book9/$', views.BookInfoViewSet.as_view({
+        'get': 'list',
+        'post': 'create',
+    })),
+]
