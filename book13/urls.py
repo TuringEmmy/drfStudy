1fc0d8ea961ea22946b381291590271a975c9fad
TuringEmmy <yonglonggeng@163.com>
2018-10-23 12:33:55 +0800

    get url config list is ok

--- book13/urls.py
+++ book13/urls.py
@@ -1,3 +1,15 @@
 # author    python
 # time      18-10-23 下午12:10
 # project   drtStudy
+from rest_framework.routers import SimpleRouter
+
+from book13 import views
+
+urlpatterns=[
+
+]
+
+
+router = SimpleRouter()
+router.register('book13',views.BookInfoViewSet,base_name = "Book13")
+urlpatterns+=router.urls
